737c8d498ecee2eacfec4364cee0fe9619017662
Marcin Michalski <mkamichalski@gmail.com>
2023-11-18 22:33:30 +0100

    update

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,16 @@
-function calculator() {}
+function calculator(a, b, parameter) {
+    switch (parameter) {
+        case '+':
+            return a + b
+        case '-':
+            return a - b
+        case '*':
+            return a * b
+        case '/':
+            return a / b
+        default:
+            console.log('error')
+    }
+}
 
 module.exports = calculator;
