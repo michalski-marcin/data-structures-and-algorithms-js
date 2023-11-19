2322d994b42c628d65eac894ea4e67ffe8a613d1
Marcin Michalski <mkamichalski@gmail.com>
2023-11-19 15:06:03 +0100

    up

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(string) {
+    let reversedString = "";
+    for (let i = 1; i <= string.length; i++) {
+        reversedString += string[string.length - i]
+    }
+    return reversedString;
+}
 
 module.exports = reverseString;
+
+
+
+
