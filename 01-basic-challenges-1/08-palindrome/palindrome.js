eddfb6db8fa5c95fcb4232d5f58e95ad00aa8624
Marcin Michalski <mkamichalski@gmail.com>
2023-11-21 21:57:52 +0100

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,14 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    let reversedString = "";
+    for (let i = 1; i <= string.length; i++) {
+        reversedString += string[string.length - i]
+    }
+    if (string.toLowerCase() === reversedString.toLowerCase()) {
+        return true;
+    } else {
+        return false
+    }
+    
+}
 
 module.exports = isPalindrome;
