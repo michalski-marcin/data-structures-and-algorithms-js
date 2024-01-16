549df907df1e8c9eba682c4eb3d4561a8ae9421c
Marcin Michalski <mkamichalski@gmail.com>
2024-01-16 20:41:47 +0100

    16.01

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
-
+function fizzBuzzArray(number) {
+    const arr = [];
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 != 0 && i % 5 != 0) {
+            arr.push(i)
+        } else if (i % 3 == 0 && i % 5 != 0) {
+            arr.push('Fizz')
+        } else if (i % 3 != 0 && i % 5 == 0) {
+            arr.push('Buzz') 
+        } else {
+            arr.push('FizzBuzz')
+        }
+    }
+    return arr
+}
+// fizzBuzzArray(102)
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+/* eslint-disable no-undef */
+function arrayIntersection(array1, array2) {
+    const intersection = [];
+
+    for (let i = 0; i < array1.length; i++) {
+        if (array2.includes(array1[i]) && !intersection.includes(array1[i])) {
+            intersection.push(array1[i])
+        }
+    }
+    return intersection
+}
+
 
 module.exports = arrayIntersection;
+
