9f1a9aa01431afcbdab7d0e3c5a3b69be4bdad45
Marcin Michalski <mkamichalski@gmail.com>
2024-02-13 20:41:48 +0100

    solution

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(arr) {
+    const length = arr.length
+    if (length === 0) {
+        return 'no one likes this'
+    } else if (length === 1) {
+        return `${arr[0]} likes this`
+    } else if (length === 2) {
+        return `${arr[0]} and ${arr[1]} like this`
+    } else if (length === 3) {
+        return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`
+    } else {
+        return `${arr[0]}, ${arr[1]} and ${length - 2} others like this`
+    }
+}
+
+
 
 module.exports = displayLikes;
